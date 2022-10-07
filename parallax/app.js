3fae9357fb22823a0862121273dc4a56e8c13502
Ellina L <ellina.dev@rambler.ru>
2022-10-08 02:15:15 +0300

    fix(parallax): speed

--- parallax/app.js
+++ parallax/app.js
@@ -1,7 +1,7 @@
 function parallax (event) {
   this.querySelectorAll('.layer').forEach(layer => {
     const speed = +layer.getAttribute('data-speed');
-    layer.style.transform = `translateX(${event.clientX*speed/1000}px)`
+    layer.style.transform = `translateX(${event.clientX*speed/1500}px)`
   });
 }
 
