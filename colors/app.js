4e829ba4ef8e8975d03e7acc75fecad2b56e544b
Ellina L <ellina.dev@rambler.ru>
2022-10-18 21:19:24 +0300

    feat(colors): set random colors

--- colors/app.js
+++ colors/app.js
@@ -1,5 +1,11 @@
 const blocks = document.querySelectorAll('.block')
 
+document.addEventListener('keydown', event => {
+  if (event.code.toLowerCase() === 'enter') {
+    setColors()
+  }
+})
+
 function generateRandomColor() {
   const hexCodes = '0123456789ABCDEF'
   let color = ''
@@ -12,8 +18,21 @@ function generateRandomColor() {
 
 function setColors() {
   blocks.forEach ((block) => {
-    block.style.background = generateRandomColor()
+    const text = block.querySelector('h2')
+    const btn = block.querySelector('button ')
+    const color = generateRandomColor()
+  
+    text.textContent = color
+    block.style.background = color
+
+    setTextColor(text, color)
+    setTextColor(btn, color)
   })
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance()
+  text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 window.addEventListener('DOMContentLoaded', () => {
