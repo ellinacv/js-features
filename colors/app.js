ca2975864a7ddce0420ba5ed4875921fe1bab31d
Ellina L <ellina.dev@rambler.ru>
2022-10-19 19:59:54 +0300

    feat(colors): colors lock

--- colors/app.js
+++ colors/app.js
@@ -1,8 +1,22 @@
 const blocks = document.querySelectorAll('.block')
 
 document.addEventListener('keydown', event => {
+  event.preventDefault()
   if (event.code.toLowerCase() === 'enter') {
     setColors()
+  }
+})
+
+document.addEventListener('click', event => {
+  const type = event.target.dataset.type
+
+  if (type === 'lock') {
+    const node = event.target.tagName.toLowerCase() === 'i'
+      ? event.target 
+      : event.target.children[0]
+
+    node.classList.toggle('fa-lock-open')
+    node.classList.toggle('fa-lock')
   }
 })
 
@@ -18,21 +32,26 @@ function generateRandomColor() {
 
 function setColors() {
   blocks.forEach ((block) => {
+    const isLocked = block.querySelector('i').classList.contains('fa-lock')
     const text = block.querySelector('h2')
-    const btn = block.querySelector('button ')
+    const btn = block.querySelector('button')
     const color = generateRandomColor()
+
+    if (isLocked) {
+      return
+    }
   
     text.textContent = color
     block.style.background = color
 
-    setTextColor(text, color)
-    setTextColor(btn, color)
+    setElementTextColor(text, color)
+    setElementTextColor(btn, color)
   })
 }
 
-function setTextColor(text, color) {
+function setElementTextColor(element, color) {
   const luminance = chroma(color).luminance()
-  text.style.color = luminance > 0.5 ? 'black' : 'white'
+  element.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 window.addEventListener('DOMContentLoaded', () => {
